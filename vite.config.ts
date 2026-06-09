a512dce1df39dcca0ed0a31f6c92b0ddc84625e5
Vishal Somaraju <vishalsomaraju9@gmail.com>
2026-06-09 20:01:55 +0530

    vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1500
+  },
   test: {
     environment: 'jsdom',
     globals: true,
